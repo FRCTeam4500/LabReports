cae5c2a359e63df79ce0e80200695b8cf3aeba3c
QuantumPie <nnewman316@gmail.com>
2016-02-09 10:18:15 -0600

    table

--- LabReports/src/main/Window.java
+++ LabReports/src/main/Window.java
@@ -1,12 +1,25 @@
 package main;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ListSelectionModel;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Window {
 
 	private JFrame frmPhysicsLabHelper;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -39,6 +52,47 @@ public class Window {
 		frmPhysicsLabHelper.setTitle("Physics Lab Helper");
 		frmPhysicsLabHelper.setBounds(100, 100, 450, 300);
 		frmPhysicsLabHelper.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPhysicsLabHelper.getContentPane().setLayout(null);
+		
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setColumnSelectionAllowed(true);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+				{null, null},
+			},
+			new String[] {
+				"X", "Y"
+			}
+		));
+		table.setBounds(0, 46, 142, 128);
+		frmPhysicsLabHelper.getContentPane().add(table);
+		
+		JLabel lblNewLabel = new JLabel("X");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(26, 22, 22, 28);
+		frmPhysicsLabHelper.getContentPane().add(lblNewLabel);
+		
+		JLabel lblY = new JLabel("Y");
+		lblY.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblY.setBounds(94, 22, 22, 28);
+		frmPhysicsLabHelper.getContentPane().add(lblY);
+		
+		JButton btnNewButton = new JButton("Submit Data");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String numone = "" + table.getValueAt(2, 1);
+				System.out.println(numone);
+			}
+		});
+		btnNewButton.setBounds(10, 185, 115, 23);
+		frmPhysicsLabHelper.getContentPane().add(btnNewButton);
 	}
-
 }
